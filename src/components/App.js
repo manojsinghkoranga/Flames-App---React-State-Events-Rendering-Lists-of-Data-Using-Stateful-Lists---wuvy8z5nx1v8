0108a25655dcab8194f9bb924d0622d8cea49406
manoj singh <117524976+manojsinghkoranga@users.noreply.github.com>
2023-01-20 08:59:57 +0000

    update

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,31 @@
 import React, {Component, useState} from "react";
 import '../styles/App.css';
 
-class App extends Component {
-    render() {
+const App = () => {
+    
+    const [firstName, setFirstName] = useState('');
+    const [secondName, setSecondName] = useState('');
 
-        return(
-            <div id="main">
-               {/* Do not remove the main div */}
-            </div>
-        )
+    const updateFirstName = (event) => {
+        setFirstName(event.target.value);
     }
+
+    const updateSecondName = (event) => {
+        setSecondName(event.target.value);
+    }
+
+    return(
+        <div id="main">
+            <input data-testid="input1" value={firstName} onChange={updateFirstName}/>
+            <input data-testid="input2" value={secondName} onChange={updateSecondName}/>
+
+            <button data-testid="calculate_relationship" onClick={calculateRelationship}>Calculate Relationship Future</button>
+            <button data-testid="clear" onClick={clearInputs}>Clear</button>
+
+            {isCalculate && <h1>{}</h1>}
+        </div>
+    )
+
 }
 
 
