048a1af10c6420a80cbfb4ac7f726979f16a2bc0
manoj singh <117524976+manojsinghkoranga@users.noreply.github.com>
2023-01-20 14:43:47 +0000

    improvement required

--- src/components/App.js
+++ src/components/App.js
@@ -6,12 +6,70 @@ const App = () => {
     const [firstName, setFirstName] = useState('');
     const [secondName, setSecondName] = useState('');
 
+    const [isCalculated, setIsCalculated] = useState('false');
+    const [relation, setRelation] = useState('');
+
     const updateFirstName = (event) => {
         setFirstName(event.target.value);
     }
 
     const updateSecondName = (event) => {
         setSecondName(event.target.value);
+    }
+
+    const relationships = (number) => {
+        console.log(typeof number);
+        switch (number.type) {
+            case 0 : {
+                return "Friends";
+            }
+            case 1 : {
+                return "Love";
+            }
+            case 2 : {
+                return "Affection";
+            }
+            case 3 : {
+                return "Marriage";
+            }
+            case 4 : {
+                return "Enemy";
+            }
+            default : {
+                return "Sibling";
+            }
+        }
+    }
+
+    const calculateRelationship = () => {
+        let firstArray = firstName.split("");
+        let secondArray = secondName.split("");
+
+        setIsCalculated(true);
+
+        if(firstArray.length === 0 || secondArray.length === 0){
+            setRelation('Please Enter valid input');
+            return;
+        }
+
+        let count = 0;
+
+        firstArray.forEach((element) => {
+            if(secondArray.includes(element)){
+                count++;
+            }
+        })
+
+        let totalLength = (firstArray.length + secondArray.length - ( count * 2 )) % 6;
+        console.log(totalLength);
+        let str = relationships(totalLength % 6);
+
+        console.log(str);
+        console.log('i reached here');
+    }
+
+    const clearInputs = () => {
+
     }
 
     return(
@@ -22,11 +80,10 @@ const App = () => {
             <button data-testid="calculate_relationship" onClick={calculateRelationship}>Calculate Relationship Future</button>
             <button data-testid="clear" onClick={clearInputs}>Clear</button>
 
-            {isCalculate && <h1>{}</h1>}
+            {isCalculated && <h3>{relation}</h3>}
         </div>
     )
 
 }
-
 
 export default App;
